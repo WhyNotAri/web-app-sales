002356c8b52be5972241aeea5a727535a4f7d1c0
Ari Morales <arimorales2506@gmail.com>
2026-02-15 12:28:57 -0600

    adding the basic service for the user entity

---
+++ src/main/java/com/ari/webapp/service/UserService.java
@@ -0,0 +1,43 @@
+package com.ari.webapp.service;
+
+import com.ari.webapp.model.Role;
+import com.ari.webapp.model.User;
+import com.ari.webapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User register(User user) {
+        if(userRepository.findByEmail(user.getEmail()).isPresent()){
+            throw new RuntimeException("Email Already Exists");
+        }
+        user.setRole(Role.USER);
+        return userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void delete(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.delete(user);
+    }
+
+    public User update(User user) {
+        User updatedUser = userRepository.findById(user.getId()).orElseThrow(() -> new RuntimeException("User not found"));
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setEmail(user.getEmail());
+        return userRepository.save(updatedUser);
+    }
+}
