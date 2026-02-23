d22fe442926fb8df2ed3861ee7da5da32ad4a99e
Ari Morales <arimorales2506@gmail.com>
2026-02-22 22:10:59 -0600

    login page

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await fetch("http://localhost:8080/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    if (!response.ok) {
+      throw new Error("Credenciales inválidas");
+    }
+
+    const data = await response.json();
+
+    window.location.href = "/products";
+
+  } catch (error) {
+    alert(error.message);
+  }
+});
